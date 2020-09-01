a8e89bdbc8f5e127c38ef264128048891d403814
vasiliyeskin <vasiliyeskin@yanex.ru>
2020-09-01 15:45:04 +0300

    Some changes in the files

--- Deep-Reinforcement-Learning-Hands-On/Chapter02/04_cartpole_random_monitor.py
+++ Deep-Reinforcement-Learning-Hands-On/Chapter02/04_cartpole_random_monitor.py
@@ -1,9 +1,9 @@
 import gym
 
-
+#for the run: xvfb-run -s "-screen 0 640x480x24" python 04_cartpole_random_monitor.py
 if __name__ == "__main__":
     env = gym.make("CartPole-v0")
-    env = gym.wrappers.Monitor(env, "recording")
+    env = gym.wrappers.Monitor(env, "recording", force=True)
 
     total_reward = 0.0
     total_steps = 0
