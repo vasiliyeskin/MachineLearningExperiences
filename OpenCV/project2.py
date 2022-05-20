c17787d6164b82d31660dfc0b8bae759600f5ade
vasiliyeskin <vasiliyeskin@yandex.ru>
2022-05-20 13:09:33 +0300

    Add projects and do it some changes in the video from the youtube

---
+++ OpenCV/project2.py
@@ -0,0 +1,121 @@
+import cv2
+import numpy as np
+
+###################################
+widthImg=640
+heightImg =360
+#####################################
+
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10, 150)
+
+
+def preProcessing(img):
+    imgGray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (5,5),1)
+    imgCanny = cv2.Canny(imgBlur,200,200)
+    kernel = np.ones((5,5))
+    imgDail = cv2.dilate(imgCanny,kernel=kernel, iterations=2)
+    imgThres = cv2.erode(imgDail, kernel, iterations=1)
+
+    return imgThres
+
+def getContours(img):
+    biggest = np.array([])
+    maxArea = 0
+    contours, hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 5000:
+            cv2.drawContours(imgContours, cnt, -1,(255,0,0),3)
+            peri = cv2.arcLength(cnt,True)
+            approx = cv2.approxPolyDP(cnt, 0.02*peri,True)
+            if area  > maxArea and len(approx) == 4:
+                biggest = approx
+                maxArea = area
+
+    cv2.drawContours(imgContours, biggest, -1, (255, 0, 0), 20)
+    return biggest
+
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4,2))
+    myPointsNew = np.zeros((4,1,2),np.int32)
+    add = myPoints.sum(1)
+
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+    diff = np.diff(myPoints,axis=1)
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+    return myPointsNew
+
+def getWarp(img, biggest):
+    biggest = reorder(biggest)
+    pts1 = np.float32(biggest)
+    pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    imgOutput = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+
+    imgCropped = imgOutput[20:imgOutput.shape[0]-20,20:imgOutput.shape[1]-20]
+    imgCropped = cv2.resize(imgCropped, (widthImg,heightImg))
+
+    return imgCropped
+
+def stackImages(scale, imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),
+                                                None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
+        ver = hor
+    return ver
+
+while True:
+    success, img = cap.read()
+    img = cv2.resize(img, (widthImg, heightImg))
+    imgContours = img.copy()
+
+    imgThres = preProcessing(img)
+    biggest = getContours(imgThres)
+    if biggest.size != 0:
+        imgWarped = getWarp(img, biggest)
+        imageArray = ([img, imgThres],[imgContours,imgWarped])
+    else:
+        imageArray = ([img, imgThres],
+                      [img, img])
+
+
+    stackImgs = stackImages(0.6, imageArray)
+
+    getContours(imgThres)
+    cv2.imshow("Video", stackImgs)
+    cv2.imshow('ImageWarped',imgWarped)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
