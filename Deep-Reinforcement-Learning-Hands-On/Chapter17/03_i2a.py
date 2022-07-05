6c3f3551094bae418f144b965652010d908b8080
veskin <vasiliyeskin@yandex.ru>
2022-07-05 23:50:06 +0300

    Add learning trajectories of net with imagitation

--- Deep-Reinforcement-Learning-Hands-On/Chapter17/03_i2a.py
+++ Deep-Reinforcement-Learning-Hands-On/Chapter17/03_i2a.py
@@ -2,6 +2,7 @@
 import os
 import ptan
 import time
+import datetime
 import gym
 import argparse
 
@@ -22,9 +23,10 @@ TEST_EVERY_BATCH = 100
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--name", required=True, help="Name of the run")
-    parser.add_argument("--cuda", default=False, action="store_true", help="Enable CUDA")
-    parser.add_argument("--em", required=True, help="Environment model file name")
+    currentDate = datetime.date.today().strftime('%d%m%Y')
+    parser.add_argument("-n", "--name", required=False, default=currentDate, help="Name of the run")
+    parser.add_argument("--cuda", default=True, action="store_true", help="Enable CUDA")
+    parser.add_argument("--em", required=False, default='saves/02_env_05072022/em_62000_5.8785e-03.dat', help="Environment model file name")
     parser.add_argument("--seed", type=int, default=common.DEFAULT_SEED, help="Random seed to use, default=%d" % common.DEFAULT_SEED)
     args = parser.parse_args()
     device = torch.device("cuda" if args.cuda else "cpu")
